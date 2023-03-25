202b51e90e47b1c3dbc477ea9f9a37819189fb29
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-25 19:12:54 +0200

    Start Shorter project. Task 1 done. Created starting classes. Added task 2.

---
+++ src/main/Shortener.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Shortener {
+    public long getId(String string) {          //gives string id
+        return 0;
+    }
+
+    public String getString(Long id) {          //gives string or null
+        return null;
+    }
+
+
+}
