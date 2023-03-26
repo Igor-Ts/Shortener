d29246268e0c454860b0edf9416c7e899ef5c019
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-26 16:55:49 +0300

    Task 3 done. Implemented shortener class methods. Added task 4.

--- src/main/Shortener.java
+++ src/main/Shortener.java
@@ -1,12 +1,28 @@
 package main;
 
+import main.strategy.StorageStrategy;
+
 public class Shortener {
-    public long getId(String string) {          //gives string id
-        return 0;
+
+    private Long lastId = 0L;                               //last id value which was used to add string in storage
+    private StorageStrategy storageStrategy;                //current storage strategy
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {          //gives string or null
-        return null;
+    public synchronized long getId(String string) {         //gives string id
+        if (storageStrategy.containsValue(string)) {        // check storage
+            return storageStrategy.getKey(string);
+        } else {                                            //add new pair in storage
+            lastId ++;
+            storageStrategy.put(lastId,string);
+            return lastId;
+        }
+    }
+
+    public synchronized String getString(Long id) {         //gives string or null
+        return storageStrategy.getValue(id);
     }
 
 
